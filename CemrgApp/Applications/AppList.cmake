20fa6357a7df781400ea1b0e8f15e51f730861bd
reflectstars <reflectstars@proton.me>
2022-05-11 12:37:29 +0000

    .versions

---
+++ CemrgApp/Applications/AppList.cmake
@@ -0,0 +1,5 @@
+option(MITK_BUILD_APP_CemrgApp "Build the MITK - CemrgApp" ON)
+# option(MITK_BUILD_APP_Workbench "Switch the MITK Workbench executable off." OFF)
+
+set(MITK_APPS
+  MainApp^^MITK_BUILD_APP_CemrgApp^^Cemr
