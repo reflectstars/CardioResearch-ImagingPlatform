d3473a41b71ed6a81de4598ea05aeca4784bf3ca
reflectstars <reflectstars@proton.me>
2022-05-14 17:29:13 +0000

    PHPCS fixes

--- CemrgApp/Modules/CemrgAppModule/cmdapps/CemrgVentricleSegmRelabel.cpp
+++ CemrgApp/Modules/CemrgAppModule/cmdapps/CemrgVentricleSegmRelabel.cpp
@@ -41,4 +41,84 @@ in the framework.
 #include <vtkDataSetSurfaceFilter.h>
 #include <vtkPolyDataConnectivityFilter.h>
 #include <vtkMath.h>
-#include <vtkFloatAr
+#include <vtkFloatArray.h>
+#include <vtkPolyData.h>
+#include <vtkPointData.h>
+#include <vtkCellData.h>
+#include <vtkPolyDataNormals.h>
+#include <vtkIdList.h>
+#include <vtkPolyDataConnectivityFilter.h>
+#include <vtkDataSetSurfaceFilter.h>
+
+// ITK
+#include <itkPoint.h>
+#include <itkImage.h>
+#include <itkImageFileWriter.h>
+#include <itkResampleImageFilter.h>
+#include <itkImageRegionIterator.h>
+#include <itkThresholdImageFilter.h>
+#include <itkBinaryThresholdImageFilter.h>
+#include <itkBinaryDilateImageFilter.h>
+#include <itkMultiplyImageFilter.h>
+#include <itkImageRegionIteratorWithIndex.h>
+#include <itkBinaryBallStructuringElement.h>
+#include <itkNearestNeighborInterpolateImageFunction.h>
+#include <itkImageRegionIterator.h>
+
+// Qt
+#include <QtDebug>
+#include <QString>
+#include <QFileInfo>
+#include <QProcess>
+#include <QMessageBox>
+
+// C++ Standard
+#include <algorithm>
+#include <string>
+#include <numeric>
+
+// CemrgApp
+#include <CemrgScar3D.h>
+#include <CemrgCommandLine.h>
+
+typedef itk::Image<uint8_t, 3> ImageType;
+typedef itk::ResampleImageFilter<ImageType, ImageType> ResampleImageFilterType;
+typedef itk::NearestNeighborInterpolateImageFunction<ImageType> NNInterpolatorType;
+typedef itk::BinaryThresholdImageFilter<ImageType, ImageType> ThresholdType;
+typedef itk::BinaryBallStructuringElement<ImageType::PixelType, 3> StrElType;
+typedef itk::BinaryDilateImageFilter<ImageType, ImageType, StrElType> ImFilterType;
+typedef itk::MultiplyImageFilter<ImageType, ImageType, ImageType> ImMultiplyType;
+typedef itk::ImageRegionIterator<ImageType> IteratorType;
+
+// ResampleImageFilterType::Pointer & imresize(ImageType::Pointer imToResize, ImageType::Pointer imTarget);
+ThresholdType::Pointer thresholdImage(ImageType::Pointer input, uint8_t thresholdVal, QString debugPrefix, bool debugging);
+ImFilterType::Pointer imdilate(ImageType::Pointer input, uint8_t radius, QString debugPrefix, bool debugging);
+ImMultiplyType::Pointer immultiply(ImageType::Pointer input1, ImageType::Pointer input2, QString debugPrefix, bool debugging);
+void relabel(ImageType::Pointer & imageToRelabel, ImageType::Pointer imageFragment, uint8_t fromLabel, uint8_t toLabel);
+
+int main(int argc, char* argv[]) {
+    mitkCommandLineParser parser;
+
+    // Set general information about your command-line app
+    parser.setCategory("EASI processing");
+    parser.setTitle("Labelled image re-labelling Command-line App");
+    parser.setContributor("CEMRG, KCL");
+    parser.setDescription("Relabel segmentation (RV/LV) for Fibres.");
+
+    // How should arguments be prefixed
+    parser.setArgumentPrefix("--", "-");
+
+    // Add arguments. Unless specified otherwise, each argument is optional.
+    // See mitkCommandLineParser::addArgument() for more information.
+    // parser.addArgument(
+    //   "input-path", "p", mitkCommandLineParser::InputFile,
+    //   "Input Directory Path", "Path of directory containing LGE files.",
+    //   us::Any(), false);
+    parser.addArgument(
+        "input", "i", mitkCommandLineParser::InputFile,
+        "Input image segmentation", "Full path of segmentation file.",
+        us::Any(), false);
+    parser.addArgument(
+        "bloodpool", "b", mitkCommandLineParser::InputFile,
+        "Input image bloodpool seg", "Full path of bloodpool file.",
+    
