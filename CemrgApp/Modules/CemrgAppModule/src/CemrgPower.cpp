2daa3ff8d51aea92c764343cb0ec5dc38d70909d
reflectstars <reflectstars@proton.me>
2022-05-20 15:59:35 +0000

    update to swift 3

--- CemrgApp/Modules/CemrgAppModule/src/CemrgPower.cpp
+++ CemrgApp/Modules/CemrgAppModule/src/CemrgPower.cpp
@@ -112,4 +112,76 @@ mitk::Surface::Pointer CemrgPower::MapPowerTransmitterToLandmarks(mitk::DataNode
     vw_ebr=norm(w1-x0);
     */
 
-    // Check that there are more than 2
+    // Check that there are more than 2 LM points selected
+    if (mps.size() >= 2) {
+        // For the MPS landmarks
+        vl_cas[0] = (mps[1][0] - mps[0][0]);
+        vl_cas[1] = (mps[1][1] - mps[0][1]);
+        vl_cas[2] = (mps[1][2] - mps[0][2]);
+        CemrgPower::normalise(vl_cas);
+    } else {
+        QMessageBox::warning(NULL, "Attention", "Select 2 or 3 landmark points");
+    }
+    // Find the rotation matrix for the vectors along length of transmitter (EBR template and MPS landmarks)
+    //Matrix3f R=Matrix3f::SetIdentity();
+    vtkSmartPointer<vtkMatrix3x3> R = vtkSmartPointer<vtkMatrix3x3>::New();
+    // Get rotation matrix between template EBR transmitter and length vector from LMs
+    CemrgPower::fcn_RotationFromTwoVectors(vl_ebr, vl_cas, R);
+
+    vtkSmartPointer<vtkMatrix4x4> R1 = vtkSmartPointer<vtkMatrix4x4>::New();
+
+    R1->SetElement(0, 0, R->GetElement(0, 0));
+    R1->SetElement(0, 1, R->GetElement(0, 1));
+    R1->SetElement(0, 2, R->GetElement(0, 2));
+    R1->SetElement(1, 0, R->GetElement(1, 0));
+    R1->SetElement(1, 1, R->GetElement(1, 1));
+    R1->SetElement(1, 2, R->GetElement(1, 2));
+    R1->SetElement(2, 0, R->GetElement(2, 0));
+    R1->SetElement(2, 1, R->GetElement(2, 1));
+    R1->SetElement(2, 2, R->GetElement(2, 2));
+
+    double vw_ebr_rot[3];
+    // Apply rotation matrix to vw_ebr vector
+    R->MultiplyPoint(vw_ebr, vw_ebr_rot);
+
+    vtkSmartPointer<vtkMatrix4x4> R2 = vtkSmartPointer<vtkMatrix4x4>::New();
+    // Find the rotation matrix for the vectors along width of transmitter (EBR template and MPS landmarks)
+    // This is an optional step if 3 landmark points are selected
+    if (mps.size() >= 3) {
+        // Re-initialise R;
+        R = vtkSmartPointer<vtkMatrix3x3>::New();
+        double vw_cas[3] = {mps[2][0] - mps[0][0], mps[2][1] - mps[0][1], mps[2][2] - mps[0][2]};
+        CemrgPower::normalise(vw_cas);
+
+        CemrgPower::fcn_RotationFromTwoVectors(vw_ebr_rot, vw_cas, R);
+
+        R2->SetElement(0, 0, R->GetElement(0, 0));
+        R2->SetElement(0, 1, R->GetElement(0, 1));
+        R2->SetElement(0, 2, R->GetElement(0, 2));
+        R2->SetElement(1, 0, R->GetElement(1, 0));
+        R2->SetElement(1, 1, R->GetElement(1, 1));
+        R2->SetElement(1, 2, R->GetElement(1, 2));
+        R2->SetElement(2, 0, R->GetElement(2, 0));
+        R2->SetElement(2, 1, R->GetElement(2, 1));
+        R2->SetElement(2, 2, R->GetElement(2, 2));
+    }
+    // We want to apply a rotation around the location of pinned location in the rib space
+
+    vtkSmartPointer<vtkMatrix4x4> vtk_T = vtkSmartPointer<vtkMatrix4x4>::New();
+    vtkSmartPointer<vtkMatrix4x4> T0 = vtkSmartPointer<vtkMatrix4x4>::New();
+    vtkSmartPointer<vtkMatrix4x4> T1 = vtkSmartPointer<vtkMatrix4x4>::New();
+    vtkSmartPointer<vtkMatrix4x4> T2 = vtkSmartPointer<vtkMatrix4x4>::New();
+    vtkSmartPointer<vtkMatrix4x4> T3 = vtkSmartPointer<vtkMatrix4x4>::New();
+
+    T0->SetElement(0, 3, -x0[0]);
+    T0->SetElement(1, 3, -x0[1]);
+    T0->SetElement(2, 3, -x0[2]);
+
+    vtkMatrix4x4::Multiply4x4(R2, T0, T1);
+    vtkMatrix4x4::Multiply4x4(R1, T1, T2);
+
+    T0->SetElement(0, 3, x0[0]);
+    T0->SetElement(1, 3, x0[1]);
+    T0->SetElement(2, 3, x0[2]);
+
+    vtkMatrix4x4::Multiply4x4(T0, T2
