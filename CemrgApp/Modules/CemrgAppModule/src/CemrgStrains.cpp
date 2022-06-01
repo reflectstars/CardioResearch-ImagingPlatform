e56acb584f52bbb1c3b61f044eb0619b4dfa42d5
reflectstars <reflectstars@proton.me>
2022-06-01 11:11:09 +0000

    - adding first batches of files.

--- CemrgApp/Modules/CemrgAppModule/src/CemrgStrains.cpp
+++ CemrgApp/Modules/CemrgAppModule/src/CemrgStrains.cpp
@@ -170,4 +170,86 @@ std::vector<double> CemrgStrains::CalculateStrainsPlot(int meshNo, mitk::DataNod
         RIV2 = lm.at(5);
         centre = Circlefit3d(ZeroPoint(lm.at(0), lm.at(1)), ZeroPoint(lm.at(0), lm.at(2)), ZeroPoint(lm.at(0), lm.at(3)));
     } else {
-      
+        RIV2 = lm.at(3);
+        centre = ZeroPoint(lm.at(0), lm.at(1));
+    }
+
+    mitk::Matrix<double, 3, 3> rotationMat = CalcRotationMatrix(centre, ZeroPoint(lm.at(0), RIV2)); RotateVTKMesh(rotationMat, surf);
+    vtkSmartPointer<vtkPolyData> pd = surf->GetVtkPolyData();
+
+    //Radial, Circumferential, and Longitudinal strains for each AHA segment
+    int index = 0;
+    std::vector<double> strainRCL(16, 0);
+
+    for (vtkIdType cellID = 0; cellID < pd->GetNumberOfCells(); cellID++) {
+
+        //Ignore non AHA segments
+        if (refCellLabels[cellID] == 0)
+            continue;
+
+        //Three nodes of the triangle
+        vtkSmartPointer<vtkCell> cell = pd->GetCell(cellID);
+        vtkSmartPointer<vtkTriangle> triangle = dynamic_cast<vtkTriangle*>(cell.GetPointer());
+        double pt1[3], pt2[3], pt3[3];
+        triangle->GetPoints()->GetPoint(0, pt1);
+        triangle->GetPoints()->GetPoint(1, pt2);
+        triangle->GetPoints()->GetPoint(2, pt3);
+
+        //Coordinate system: vectors of the triangle
+        mitk::Point3D vc1, vc2, vc3;
+        vc1.SetElement(0, pt2[0] - pt1[0]);
+        vc1.SetElement(1, pt2[1] - pt1[1]);
+        vc1.SetElement(2, pt2[2] - pt1[2]);
+        vc2.SetElement(0, pt3[0] - pt1[0]);
+        vc2.SetElement(1, pt3[1] - pt1[1]);
+        vc2.SetElement(2, pt3[2] - pt1[2]);
+        vc3.SetElement(0, Cross(vc1, vc2).GetElement(0) / Norm(Cross(vc1, vc2)));
+        vc3.SetElement(1, Cross(vc1, vc2).GetElement(1) / Norm(Cross(vc1, vc2)));
+        vc3.SetElement(2, Cross(vc1, vc2).GetElement(2) / Norm(Cross(vc1, vc2)));
+
+        //Assemble K matrix
+        mitk::Matrix<double, 3, 3> K;
+        K[0][0] = vc1.GetElement(0);
+        K[0][1] = vc2.GetElement(0);
+        K[0][2] = vc3.GetElement(0);
+        K[1][0] = vc1.GetElement(1);
+        K[1][1] = vc2.GetElement(1);
+        K[1][2] = vc3.GetElement(1);
+        K[2][0] = vc1.GetElement(2);
+        K[2][1] = vc2.GetElement(2);
+        K[2][2] = vc3.GetElement(2);
+
+        //Calculate deformation gradient
+        mitk::Matrix<double, 3, 3> F;
+        F = K * refJ.at(index).GetInverse();
+
+        //Calculate Strain Tensors
+        mitk::Matrix<double, 3, 3> ET;
+        mitk::Matrix<double, 3, 3> EYE; EYE.SetIdentity();
+
+        //Green-Lagrange or Engineering
+        if (flag > 2)
+            ET = 0.5 * (F.GetTranspose() * F.GetVnlMatrix() - EYE.GetVnlMatrix());
+        else
+            ET = 0.5 * (F.GetVnlMatrix() + F.GetTranspose()) - EYE.GetVnlMatrix();
+
+        //Rotate Green-Lagrange strain
+        mitk::Matrix<double, 3, 3> E;
+        E = refQ.at(index) * ET * refQ.at(index).GetTranspose();
+
+        //Store symmetric Green-Lagrange strain in Voigt notation
+        mitk::Matrix<double, 1, 3> EV;
+        EV[0][0] = E(0, 0);
+        EV[0][1] = E(1, 1);
+        EV[0][2] = E(2, 2);
+
+        //Prepare plot values
+        strainRCL.at(refCellLabels[cellID] - 1) += EV[0][(flag > 2) ? flag - 2 : flag];
+        flatSurfScalars->InsertTuple1(index, EV[0][(flag > 2) ? flag - 2 : flag]);
+
+        index++;
+    }//_for
+
+    for (int i = 0; i < 16; i++)
+        strainRCL.at(i) /= std::count(refCellLabels.begin(), refCellLabels.end(), i + 1);
+
