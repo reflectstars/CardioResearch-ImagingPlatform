84cc960b8d18e23007542b4e321d497e093bc646
reflectstars <reflectstars@proton.me>
2022-06-26 14:44:47 +0000

    re-org

--- CemrgApp/Modules/CemrgAppModule/test/test.hpp
+++ CemrgApp/Modules/CemrgAppModule/test/test.hpp
@@ -14,4 +14,34 @@ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
-/*==
+/*=========================================================================
+ *
+ * CEMRGAPPMODULE TESTS
+ *
+ * Cardiac Electromechanics Research Group
+ * http://www.cemrg.com
+ * orod.razeghi@kcl.ac.uk
+ *
+ * This software is distributed WITHOUT ANY WARRANTY or SUPPORT!
+ *
+=========================================================================*/
+
+#include <QtTest/QtTest>
+
+// An example test class
+class TestSuite : public QObject {
+
+    Q_OBJECT
+
+private:
+    bool myCondition();
+
+private slots:
+    void initTestCase();
+
+    void myFirstTest();
+
+    void mySecondTest();
+
+    void cleanupTestCase();
+};
