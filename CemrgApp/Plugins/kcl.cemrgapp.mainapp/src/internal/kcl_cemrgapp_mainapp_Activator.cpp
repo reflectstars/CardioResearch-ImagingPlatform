61db7ebe7b64a8a99a128ec6a0cb5e78d766b50c
reflectstars <reflectstars@proton.me>
2022-07-01 22:38:52 +0000

    add travis ci support

--- CemrgApp/Plugins/kcl.cemrgapp.mainapp/src/internal/kcl_cemrgapp_mainapp_Activator.cpp
+++ CemrgApp/Plugins/kcl.cemrgapp.mainapp/src/internal/kcl_cemrgapp_mainapp_Activator.cpp
@@ -63,4 +63,51 @@ namespace mitk {
 
         BERRY_REGISTER_EXTENSION_CLASS(QmitkCemrgApplication, context);
         BERRY_REGISTER_EXTENSION_CLASS(QmitkCemrgAppCommonTools, context);
-        BERRY_
+        BERRY_REGISTER_EXTENSION_CLASS(QmitkCemrgJBPerspective, context);
+        BERRY_REGISTER_EXTENSION_CLASS(QmitkCemrgHCPerspective, context);
+        BERRY_REGISTER_EXTENSION_CLASS(QmitkCemrgRRPerspective, context);
+        BERRY_REGISTER_EXTENSION_CLASS(QmitkCemrgEasiPerspective, context);
+        BERRY_REGISTER_EXTENSION_CLASS(QmitkCemrgPowertransPerspective, context);
+        BERRY_REGISTER_EXTENSION_CLASS(QmitkCemrgWathcaPerspective, context);
+
+        // QString collectionFile = GetQtHelpCollectionFile();
+        // berry::QtAssistantUtil::SetHelpCollectionFile(collectionFile);
+        // berry::QtAssistantUtil::SetDefaultHelpUrl("qthelp://kcl.cemrgapp.cemrgapp/bundle/index.html");
+    }
+
+    ctkPluginContext* kcl_cemrgapp_mainapp_Activator::GetPluginContext() const {
+        return context;
+    }
+
+    QString kcl_cemrgapp_mainapp_Activator::GetQtHelpCollectionFile() const {
+
+        if (!helpCollectionFile.isEmpty()) {
+            return helpCollectionFile;
+        }
+
+        QString collectionFilename = "CemrgAppQtHelpCollection.qhc";
+        QFileInfo collectionFileInfo = context->getDataFile(collectionFilename);
+        QFileInfo pluginFileInfo = QFileInfo(QUrl(context->getPlugin()->getLocation()).toLocalFile());
+
+        if (!collectionFileInfo.exists() || pluginFileInfo.lastModified()
+            > collectionFileInfo.lastModified()) {
+
+            // extract the qhc file from the plug-in
+            QByteArray content = context->getPlugin()->getResource(collectionFilename);
+            if (content.isEmpty()) {
+                BERRY_WARN << "Could not get plug-in resource: " << collectionFilename.toStdString();
+            } else {
+                QFile file(collectionFileInfo.absoluteFilePath());
+                file.open(QIODevice::WriteOnly);
+                file.write(content);
+                file.close();
+            }
+        }
+
+        if (QFile::exists(collectionFileInfo.absoluteFilePath())) {
+            helpCollectionFile = collectionFileInfo.absoluteFilePath();
+        }
+
+        return helpCollectionFile;
+    }
+}
