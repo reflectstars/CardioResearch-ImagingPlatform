e4325e95de4db51012b09e22eb434f99fa1ef0b2
reflectstars <reflectstars@proton.me>
2022-07-08 14:16:18 +0000

    Update approved.yaml

---
+++ CemrgApp/Plugins/kcl.cemrgapp.mmeasurement/src/internal/MmeasurementView.cpp
@@ -0,0 +1,42 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+/*=========================================================================
+ *
+ * Anatomical Measurements
+ *
+ * Cardiac Electromechanics Research Group
+ * http://www.cemrgapp.com
+ * orod.razeghi@kcl.ac.uk
+ *
+ * This software is distributed WITHOUT ANY WARRANTY or SUPPORT!
+ *
+=========================================================================*/
+
+// Blueberry
+#include <berryISelectionService.h>
+#include <berryIWorkbenchWindow.h>
+#include <berryIWorkbenchPage.h>
+#include <berryFileEditorInput.h>
+
+// Qmitk
+#include <QmitkIOUtil.h>
+#include <mitkCuboid.h>
+#include <mitkDataNode.h>
+#include <mitkProgressBar.h>
+#include <mitkIDataStorageService.h>
+#include <mitkNodePredicateNot.h>
+#include 
