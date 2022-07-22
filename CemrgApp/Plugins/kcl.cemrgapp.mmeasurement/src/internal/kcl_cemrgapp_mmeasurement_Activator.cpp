6aecd491114b4f2726aa1f844907a6fa85ebc0fb
reflectstars <reflectstars@proton.me>
2022-07-22 15:48:20 +0000

    Setting version to 0.8.0

---
+++ CemrgApp/Plugins/kcl.cemrgapp.mmeasurement/src/internal/kcl_cemrgapp_mmeasurement_Activator.cpp
@@ -0,0 +1,10 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/
