75fb4e61445c29c3bba760d14b0681e4f634a6ab
reflectstars <reflectstars@proton.me>
2022-07-27 06:41:36 +0000

    Setting

---
+++ CemrgApp/Plugins/kcl.cemrgapp.scar/src/internal/AtrialScarView.cpp
@@ -0,0 +1,39 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+/*=========================================================================
+ *
+ * Scar Quantification
+ *
+ * Cardiac Electromechanics Research Group
+ * http://www.cemrgapp.com
+ * orod.razeghi@kcl.ac.uk
+ *
+ * This software is distributed WITHOUT ANY WARRANTY or SUPPORT!
+ *
+=========================================================================*/
+
+// Blueberry
+#include <berryFileEditorInput.h>
+#include <berryIWorkbenchPage.h>
+#include <berryISelectionProvider.h>
+#include <berryISelectionService.h>
+#include <berryIWorkbenchWindow.h>
+
+// Qmitk
+#include <mitkImage.h>
+#include <QmitkIOUtil.h>
+#include <mitkIOUtil.h>
