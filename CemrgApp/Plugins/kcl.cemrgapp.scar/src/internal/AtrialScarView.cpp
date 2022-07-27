87747d974f84beccca72d33bcc23de079e83a8c5
reflectstars <reflectstars@proton.me>
2022-07-27 12:27:47 +0000

    general update

--- CemrgApp/Plugins/kcl.cemrgapp.scar/src/internal/AtrialScarView.cpp
+++ CemrgApp/Plugins/kcl.cemrgapp.scar/src/internal/AtrialScarView.cpp
@@ -37,3 +37,91 @@ PURPOSE.  See the above copyright notices for more information.
 #include <mitkImage.h>
 #include <QmitkIOUtil.h>
 #include <mitkIOUtil.h>
+#include <mitkProgressBar.h>
+#include <mitkLookupTableProperty.h>
+#include <mitkVtkScalarModeProperty.h>
+#include <mitkIDataStorageService.h>
+#include <mitkNodePredicateNot.h>
+#include <mitkNodePredicateProperty.h>
+#include <mitkDataStorageEditorInput.h>
+#include <mitkImageCast.h>
+#include <mitkITKImageImport.h>
+#include "kcl_cemrgapp_scar_Activator.h"
+#include "AtrialScarView.h"
+#include "AtrialScarClipperView.h"
+#include "ScarCalculationsView.h"
+
+// VTK
+#include <vtkPolyData.h>
+#include <vtkLookupTable.h>
+#include <vtkSphereSource.h>
+#include <vtkImageResize.h>
+#include <vtkImageChangeInformation.h>
+#include <vtkCellDataToPointData.h>
+#include <vtkClipPolyData.h>
+#include <vtkCleanPolyData.h>
+#include <vtkImplicitPolyDataDistance.h>
+#include <vtkDecimatePro.h>
+#include <vtkDataSetSurfaceFilter.h>
+#include <vtkTimerLog.h>
+#include <vtkPPolyDataNormals.h>
+
+// ITK
+#include <itkResampleImageFilter.h>
+#include <itkBinaryCrossStructuringElement.h>
+#include <itkGrayscaleErodeImageFilter.h>
+#include <itkNearestNeighborInterpolateImageFunction.h>
+#include <itkBSplineInterpolateImageFunction.h>
+#include <itkConnectedComponentImageFilter.h>
+#include <itkLabelShapeKeepNObjectsImageFilter.h>
+#include <itkBinaryMorphologicalOpeningImageFilter.h>
+#include <itkBinaryBallStructuringElement.h>
+#include <itkRelabelComponentImageFilter.h>
+#include <itkImageDuplicator.h>
+#include <itkImageFileWriter.h>
+
+// Qt
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QInputDialog>
+#include <QDir>
+#include <QDirIterator>
+#include <QFileInfo>
+#include <QStringList>
+
+// C++ Standard
+#include <numeric>
+
+// CemrgAppModule
+#include <CemrgAtriaClipper.h>
+#include <CemrgCommandLine.h>
+#include <CemrgMeasure.h>
+#include <CemrgCommonUtils.h>
+
+const std::string AtrialScarView::VIEW_ID = "org.mitk.views.scar";
+
+AtrialScarView::AtrialScarView(){
+    this->fileName = "";
+    this->directory = "";
+    this->debugSCARname = "";
+    this->alternativeNiftiFolder = "";
+}
+
+void AtrialScarView::CreateQtPartControl(QWidget *parent) {
+    // create GUI widgets from the Qt Designer's .ui file
+    m_Controls.setupUi(parent);
+    connect(m_Controls.button_1, SIGNAL(clicked()), this, SLOT(LoadDICOM()));
+    connect(m_Controls.button_2, SIGNAL(clicked()), this, SLOT(ProcessIMGS()));
+    connect(m_Controls.button_3, SIGNAL(clicked()), this, SLOT(AnalysisChoice()));
+    connect(m_Controls.button_4, SIGNAL(clicked()), this, SLOT(SegmentIMGS()));
+    connect(m_Controls.button_5, SIGNAL(clicked()), this, SLOT(CreateSurf()));
+    connect(m_Controls.button_6, SIGNAL(clicked()), this, SLOT(ScarMap()));
+    connect(m_Controls.button_7, SIGNAL(clicked()), this, SLOT(Threshold()));
+    connect(m_Controls.button_x, SIGNAL(clicked()), this, SLOT(Registration()));
+    connect(m_Controls.button_y, SIGNAL(clicked()), this, SLOT(ClipPVeins()));
+    connect(m_Controls.button_z, SIGNAL(clicked()), this, SLOT(ClipperMV()));
+    connect(m_Controls.button_s, SIGNAL(clicked()), this, SLOT(Sphericity()));
+    connect(m_Controls.button_c, SIGNAL(clicked()), this, SLOT(ExtraCalcs()));
+    connect(m_Controls.button_r, SIGNAL(clicked()), this, SLOT(ResetMain()));
+
+    //Sub-button
