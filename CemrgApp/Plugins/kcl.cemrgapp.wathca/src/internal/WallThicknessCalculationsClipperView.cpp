1f53d45ba8c508b24f10c5dd80e5cfb929b3ca31
reflectstars <reflectstars@proton.me>
2022-09-12 20:37:42 +0000

    Fix jshint issues

---
+++ CemrgApp/Plugins/kcl.cemrgapp.wathca/src/internal/WallThicknessCalculationsClipperView.cpp
@@ -0,0 +1,41 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+/*=========================================================================
+ *
+ * Morphological Quantification
+ *
+ * Cardiac Electromechanics Research Group
+ * http://www.cemrgapp.com
+ * orod.razeghi@kcl.ac.uk
+ *
+ * This software is distributed WITHOUT ANY WARRANTY or SUPPORT!
+ *
+=========================================================================*/
+
+// Blueberry
+#include <berryISelectionService.h>
+#include <berryIWorkbenchWindow.h>
+#include <berryIWorkbenchPage.h>
+
+// Qmitk
+#include <mitkImage.h>
+#include <mitkIOUtil.h>
+#include <mitkProgressBar.h>
+#include <mitkNodePredicateProperty.h>
+#include <mitkManualSegmentationToSurfaceFilter.h>
+#include "WallThicknessCalculationsClipperView.h"
+#include "WallThicknessCalculationsV
