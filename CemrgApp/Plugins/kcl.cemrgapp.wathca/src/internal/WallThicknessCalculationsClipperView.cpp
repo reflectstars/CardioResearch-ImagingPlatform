dc2a23dc740a0de69c37d9adc9e3cf0a5cca15e9
reflectstars <reflectstars@proton.me>
2022-09-16 06:29:35 +0000

    1st post

--- CemrgApp/Plugins/kcl.cemrgapp.wathca/src/internal/WallThicknessCalculationsClipperView.cpp
+++ CemrgApp/Plugins/kcl.cemrgapp.wathca/src/internal/WallThicknessCalculationsClipperView.cpp
@@ -38,4 +38,85 @@ PURPOSE.  See the above copyright notices for more information.
 #include <mitkNodePredicateProperty.h>
 #include <mitkManualSegmentationToSurfaceFilter.h>
 #include "WallThicknessCalculationsClipperView.h"
-#include "WallThicknessCalculationsV
+#include "WallThicknessCalculationsView.h"
+
+// VTK
+#include <vtkGlyph3D.h>
+#include <vtkSphereSource.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkProperty.h>
+#include <vtkCellPicker.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkCell.h>
+#include <vtkMath.h>
+#include <vtkDoubleArray.h>
+#include <vtkPointData.h>
+#include <vtkTextActor.h>
+#include <vtkTextProperty.h>
+#include <vtkCleanPolyData.h>
+#include <vtkPolyDataNormals.h>
+#include <vtkWindowedSincPolyDataFilter.h>
+#include <vtkPolyDataConnectivityFilter.h>
+
+// ITK
+#include <itkAddImageFilter.h>
+#include <itkRelabelComponentImageFilter.h>
+#include <itkConnectedComponentImageFilter.h>
+#include <itkImageRegionIteratorWithIndex.h>
+#include "itkLabelObject.h"
+#include "itkLabelMap.h"
+#include "itkLabelImageToLabelMapFilter.h"
+#include "itkLabelMapToLabelImageFilter.h"
+#include "itkLabelSelectionLabelMapFilter.h"
+
+// Qt
+#include <QMessageBox>
+#include <QDesktopWidget>
+
+// CemrgAppModule
+#include <CemrgAtriaClipper.h>
+#include <CemrgCommandLine.h>
+
+QString WallThicknessCalculationsClipperView::fileName;
+QString WallThicknessCalculationsClipperView::directory;
+const std::string WallThicknessCalculationsClipperView::VIEW_ID = "org.mitk.views.wathcaclipperview";
+
+WallThicknessCalculationsClipperView::WallThicknessCalculationsClipperView() {
+    this->inputs = new QDialog(0, 0);
+}
+
+void WallThicknessCalculationsClipperView::CreateQtPartControl(QWidget *parent) {
+
+    // create GUI widgets from the Qt Designer's .ui file
+    m_Controls.setupUi(parent);
+    connect(m_Controls.button_1, SIGNAL(clicked()), this, SLOT(CtrLines()));
+    connect(m_Controls.button_2, SIGNAL(clicked()), this, SLOT(CtrPlanes()));
+    connect(m_Controls.button_4, SIGNAL(clicked()), this, SLOT(ClipperImage()));
+    connect(m_Controls.slider, SIGNAL(valueChanged(int)), this, SLOT(CtrPlanesPlacer()));
+    connect(m_Controls.spinBox, SIGNAL(valueChanged(double)), this, SLOT(CtrPlanesPlacer()));
+    connect(m_Controls.comboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(CtrLinesSelector(int)));
+
+    //Create GUI widgets
+    inputs = new QDialog(0, 0);
+    m_Labels.setupUi(inputs);
+    connect(m_Labels.buttonBox, SIGNAL(accepted()), inputs, SLOT(accept()));
+    connect(m_Labels.buttonBox, SIGNAL(rejected()), inputs, SLOT(reject()));
+
+    //Setup renderer
+    surfActor = vtkSmartPointer<vtkActor>::New();
+    renderer = vtkSmartPointer<vtkRenderer>::New();
+    renderer->SetBackground(0, 0, 0);
+    vtkSmartPointer<vtkTextActor> txtActor = vtkSmartPointer<vtkTextActor>::New();
+    std::string shortcuts = "R: reset centrelines\nSpace: add seed point\nDelete: remove seed point";
+    txtActor->SetInput(shortcuts.c_str());
+    txtActor->GetTextProperty()->SetFontSize(14);
+    txtActor->GetTextProperty()->SetColor(1.0, 1.0, 1.0);
+    renderer->AddActor2D(txtActor);
+
+    vtkSmartPointer<vtkGenericOpenGLRenderWindow> renderWindow = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
+    m_Controls.widget_1->SetRenderWindow(renderWindow);
+    m_Controls.widget_1->GetRenderWindow()->AddRenderer(renderer);
+
+    //Setup 
