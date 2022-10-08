5f3a7549ff169032f15e0a7ca6ef678eb5680ce8
reflectstars <reflectstars@proton.me>
2022-10-08 09:52:20 +0000

    drm/i915: add some barriers when changing DIPs

--- CemrgApp/Plugins/kcl.cemrgapp.wathca/src/internal/WallThicknessCalculationsView.cpp
+++ CemrgApp/Plugins/kcl.cemrgapp.wathca/src/internal/WallThicknessCalculationsView.cpp
@@ -64,4 +64,80 @@ PURPOSE.  See the above copyright notices for more information.
 #endif
 
 // VTK
-#include 
+#include <vtkFieldData.h>
+#include <vtkCleanPolyData.h>
+#include <vtkPolyDataNormals.h>
+#include <vtkPolyDataConnectivityFilter.h>
+#include <vtkWindowedSincPolyDataFilter.h>
+
+// ITK
+#include <itkAddImageFilter.h>
+#include <itkRelabelComponentImageFilter.h>
+#include <itkConnectedComponentImageFilter.h>
+#include <itkImageRegionIteratorWithIndex.h>
+#include "itkLabelObject.h"
+#include "itkLabelMap.h"
+#include "itkLabelImageToLabelMapFilter.h"
+#include "itkLabelMapToLabelImageFilter.h"
+#include "itkLabelSelectionLabelMapFilter.h"
+
+// Qt
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QInputDialog>
+
+// CemrgAppModule
+#include <CemrgCommonUtils.h>
+#include <CemrgCommandLine.h>
+#include <CemrgMeasure.h>
+
+const std::string WallThicknessCalculationsView::VIEW_ID = "org.mitk.views.wathcaview";
+
+WallThicknessCalculationsView::WallThicknessCalculationsView(){
+    this->fileName = "";
+    this->directory = "";
+}
+
+void WallThicknessCalculationsView::CreateQtPartControl(QWidget *parent) {
+
+    // create GUI widgets from the Qt Designer's .ui file
+    m_Controls.setupUi(parent);
+    connect(m_Controls.button_1, SIGNAL(clicked()), this, SLOT(LoadDICOM()));
+    connect(m_Controls.button_2, SIGNAL(clicked()), this, SLOT(ProcessIMGS()));
+    connect(m_Controls.button_3, SIGNAL(clicked()), this, SLOT(SegmentIMGS()));
+    connect(m_Controls.button_4, SIGNAL(clicked()), this, SLOT(ClipperPV()));
+    connect(m_Controls.button_5, SIGNAL(clicked()), this, SLOT(MorphologyAnalysis()));
+    connect(m_Controls.button_6, SIGNAL(clicked()), this, SLOT(ThicknessAnalysis()));
+    connect(m_Controls.button_r, SIGNAL(clicked()), this, SLOT(Reset()));
+
+    //Set visibility of buttons
+    m_Controls.button_2_1->setVisible(false);
+    m_Controls.button_2_2->setVisible(false);
+    m_Controls.button_2_3->setVisible(false);
+    m_Controls.button_2_4->setVisible(false);
+    connect(m_Controls.button_2_1, SIGNAL(clicked()), this, SLOT(ConvertNII()));
+    connect(m_Controls.button_2_2, SIGNAL(clicked()), this, SLOT(CropIMGS()));
+    connect(m_Controls.button_2_3, SIGNAL(clicked()), this, SLOT(ResampIMGS()));
+    connect(m_Controls.button_2_4, SIGNAL(clicked()), this, SLOT(ApplyFilter()));
+    m_Controls.button_3_1->setVisible(false);
+    m_Controls.button_3_2->setVisible(false);
+    m_Controls.button_3_3->setVisible(false);
+    connect(m_Controls.button_3_1, SIGNAL(clicked()), this, SLOT(SelectROI()));
+    connect(m_Controls.button_3_2, SIGNAL(clicked()), this, SLOT(SelectLandmarks()));
+    connect(m_Controls.button_3_3, SIGNAL(clicked()), this, SLOT(CombineSegs()));
+    m_Controls.button_6_1->setVisible(false);
+    m_Controls.button_6_2->setVisible(false);
+    connect(m_Controls.button_6_1, SIGNAL(clicked()), this, SLOT(ConvertNRRD()));
+    connect(m_Controls.button_6_2, SIGNAL(clicked()), this, SLOT(ThicknessCalculator()));
+}
+
+void WallThicknessCalculationsView::SetFocus() {
+
+    m_Controls.button_1->setFocus();
+}
+
+void WallThicknessCalculationsView::OnSelectionChanged(
+        berry::IWorkbenchPart::Pointer /*source*/, const QList<mitk::DataNode::Pointer>& /*nodes*/) {
+}
+
+void WallThicknessCalculationsView::Lo
